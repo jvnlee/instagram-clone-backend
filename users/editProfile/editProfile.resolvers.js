8e248b50ddec472f701ffadc5ff68fdad398403a
jvnlee <lhjun1028@naver.com>
2021-09-28 21:37:44 +0900

    Back-end with Apollo server

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,22 +1,21 @@
 import client from "../../client";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 
 export default {
   Mutation: {
     editProfile: async (
       _,
-      { firstName, lastName, username, email, password: newPassword, token }
+      { firstName, lastName, username, email, password: newPassword },
+      { loggedInUser, protectResolver }
     ) => {
-      // STEP1: take the changed data from the user, and if the password has a change, hash it.
-      const { id } = await jwt.verify(token, process.env.SECRET_KEY);
+      protectResolver(loggedInUser);
       let hashedPassword = null;
       if (newPassword) {
         hashedPassword = await bcrypt.hash(newPassword, 10);
       }
       const updatedUser = await client.user.update({
         where: {
-          id,
+          id: loggedInUser.id,
         },
         data: {
           firstName,
@@ -26,6 +25,7 @@ export default {
           ...(hashedPassword && { password: hashedPassword }),
         },
       });
+      console.log(updatedUser);
       if (updatedUser.id) {
         return {
           status: true,
